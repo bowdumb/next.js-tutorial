e983a5cb14ce30ac5694a47541c4c41d2688e1e0
Josh Claxton <125156252+bowdumb@users.noreply.github.com>
2024-02-01 11:27:36 -0500

    resolving merge conflicts for database connection

--- app/ui/dashboard/nav-links.tsx
+++ app/ui/dashboard/nav-links.tsx
@@ -22,7 +22,8 @@ const links = [
 ];
 
 export default function NavLinks() {
-  const pathname = usePathname();''
+  const pathname = usePathname();
+
   return (
     <>
       {links.map((link) => {
